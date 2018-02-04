ec083c379887a2979dc0cecb291e6cf7f8d2e186
Ewout Prangsma <ewout@prangsma.net>
2018-02-04 12:57:56 +0100

    Draw direction marker for more block states

--- WinApp/Items/Run/BlockItem.cs
+++ WinApp/Items/Run/BlockItem.cs
@@ -58,23 +58,46 @@ namespace BinkyRailways.WinApp.Items.Run
 
             var blockState = state.State;
             var loc = state.LockedBy;
-            if ((loc != null) && (loc.CurrentBlock.Actual == state))
+            var reverse = Entity.ReverseSides;
+            switch (blockState)
             {
-                var reverse = Entity.ReverseSides;
-                if (loc.CurrentBlockEnterSide.Actual == BlockSide.Front)
-                {
-                    // If loc entered block at front, we go towards back.
-                    reverse = !reverse;
-                }
-                // Draw "loc direction" marker
-                using (var path = new GraphicsPath())
-                {
-                    var radius = (Math.Min(sz.Width, sz.Height) * 1.0f);
-                    var xOffset = 0.0f;// radius * 0.5f;
-                    var p1 = new PointF(reverse ? -xOffset : sz.Width + xOffset, sz.Height / 2.0f);
-                    var brush = loc.PossibleDeadlock.Actual ? Brushes.Orange : Brushes.White;
-                    e.Graphics.FillEllipse(brush, new RectangleF(new PointF(p1.X - (radius / 4.0f), p1.Y - (radius / 4.0f)), new SizeF(radius / 2.0f, radius / 2.0f)));
-                }
+                case BlockState.Occupied:
+                    if ((loc != null) && (loc.CurrentBlock.Actual == state))
+                    {
+                        if (loc.CurrentBlockEnterSide.Actual == BlockSide.Front)
+                        {
+                            // If loc entered block at front, we go towards back.
+                            reverse = !reverse;
+                        }
+                        // Draw "loc direction" marker
+                        drawLocDirectionMarker(e, sz, reverse, loc);
+                    }
+                    break;
+                case BlockState.Entering:
+                case BlockState.Destination:
+                    if ((loc != null) && (loc.CurrentRoute.Actual != null))
+                    {
+                        if (loc.CurrentRoute.Actual.Route.ToBlockSide == BlockSide.Front)
+                        {
+                            // If loc will block at front, we go towards back.
+                            reverse = !reverse;
+                        }
+                        // Draw "loc direction" marker
+                        drawLocDirectionMarker(e, sz, reverse, loc);
+                    }
+                    break;
+            }
+        }
+
+        private void drawLocDirectionMarker(ItemPaintEventArgs e, Size sz, bool reverse, ILocState loc)
+        {
+            using (var path = new GraphicsPath())
+            {
+                var radius = (Math.Min(sz.Width, sz.Height) * 1.0f);
+                var xOffset = 0.0f;// radius * 0.5f;
+                var p1 = new PointF(reverse ? -xOffset : sz.Width + xOffset, sz.Height / 2.0f);
+                var brush = loc.PossibleDeadlock.Actual ? Brushes.Orange : Brushes.White;
+                e.Graphics.FillEllipse(brush, new RectangleF(new PointF(p1.X - (radius / 4.0f), p1.Y - (radius / 4.0f)), new SizeF(radius / 2.0f, radius / 2.0f)));
             }
         }
 
