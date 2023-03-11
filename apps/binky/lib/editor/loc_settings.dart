35131488dc5e3cfe21bf8403b12956b70d3148c3
Ewout Prangsma <ewout@prangsma.net>
2023-03-11 17:08:30 +0100

    Improved UI layout for loc image editing

--- apps/binky/lib/editor/loc_settings.dart
+++ apps/binky/lib/editor/loc_settings.dart
@@ -187,19 +187,25 @@ class _LocSettingsState extends State<_LocSettings> {
             },
             items: BinkyDropdownMenuItems.ChangeDirectionItems,
           ),
-          const SettingsHeader(title: "Image"),
-          IconButton(
-            onPressed: () async {
-              final result = await FilePicker.platform
-                  .pickFiles(type: FileType.image, withData: true);
-              if (result != null) {
-                final data = result.files.single.bytes;
-                if (data != null) {
-                  await widget.model.updateLocImage(widget.loc, data.toList());
-                }
-              }
-            },
-            icon: Icon(Icons.file_upload),
+          Row(
+            children: [
+              const SettingsHeader(title: "Image"),
+              Expanded(child: Container()),
+              IconButton(
+                onPressed: () async {
+                  final result = await FilePicker.platform
+                      .pickFiles(type: FileType.image, withData: true);
+                  if (result != null) {
+                    final data = result.files.single.bytes;
+                    if (data != null) {
+                      await widget.model
+                          .updateLocImage(widget.loc, data.toList());
+                    }
+                  }
+                },
+                icon: const Icon(Icons.file_upload),
+              ),
+            ],
           ),
           hasImage ? Image.network(widget.loc.imageUrl) : Container(),
         ],
